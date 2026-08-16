012f2f62aeeb63d40c674099aeba3583f42fd75e
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-08-16 22:08:40 +0200

    add cmp flags

---
+++ Turing.Core/Computer/Symphony/CMP_FLAGS.cs
@@ -0,0 +1,21 @@
+﻿using Turing.Core.ComplexComponents;
+using Turing.Core.Electricity;
+
+namespace Turing.Core.Computer.Symphony;
+
+internal class CMP_FLAGS(Int A, Int B) : TurComponentValue<Int>
+{
+    protected override Int ImplicitOperator()
+    {
+        Bit eq = new EQ<Int>(A, B);
+        Bit low = new LOW<Int>(A, B);
+        Bit less = new LESS<Int>(A, B);
+
+        Int result = new Int();
+        result.SetBit(0, eq);
+        result.SetBit(1, low);
+        result.SetBit(2, less);
+
+        return result;
+    }
+}
