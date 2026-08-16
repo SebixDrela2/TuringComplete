99b549e85d3d8b44b134ef8d39dc52860047eb66
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-08-16 05:33:51 +0200

    add counter4

---
+++ Turing.Core/Components/Memory/COUNTER4.cs
@@ -0,0 +1,59 @@
+﻿using Turing.Core.Components.Logic;
+using Turing.Core.Electricity;
+using Turing.Core.Gates.Primitives;
+
+public class COUNTER4<T> : IStateGate<T> where T : struct, IValue<T>
+{
+    private readonly REGISTER<T> _register;
+    public readonly CLOCK _clock;
+    private readonly CONST<T> _one;
+
+    public T State => _register.State;
+
+    public COUNTER4(CLOCK clock)
+    {
+        _clock = clock;
+        _register = new REGISTER<T>(_clock);
+        _one = new CONST<T>(4);
+    }
+
+    public COUNTER4(T initialValue, CLOCK clock)
+    {
+        _clock = clock;
+        _register = new REGISTER<T>(_clock);
+        _one = new CONST<T>(4);
+        Load(initialValue);
+    }
+
+    public void EVal(Bit load, T loadValue)
+    {
+        T currentState = _register.State;
+        T one = (T)_one;
+        T incremented = Add(currentState, one);
+        T muxResult = new MUX<T>(incremented, loadValue, load);
+
+        _register.EVal(_clock, muxResult);
+    }
+
+    public void Load(T value)
+    {
+        _register.EVal(new Bit(true), value);
+    }
+
+    public void Reset()
+    {
+        _register.Reset();
+    }
+
+    public static implicit operator T(COUNTER4<T> counter)
+    {
+        return counter.State;
+    }
+
+    private static T Add(T a, T b)
+    {
+        T zero = a.FromValue(false);
+        (T Sum, Bit Carry) result = new ADDER<T>(a, b, zero);
+        return result.Sum;
+    }
+}
