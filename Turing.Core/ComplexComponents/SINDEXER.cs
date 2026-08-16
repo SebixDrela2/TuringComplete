ff1a0fa9b51eeb82ee54270ce42f81da59eab8d6
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-08-16 18:39:32 +0200

    add static indexer

---
+++ Turing.Core/ComplexComponents/SINDEXER.cs
@@ -0,0 +1,24 @@
+﻿using Turing.Core.Components.Arithmetic;
+using Turing.Core.Components.Logic;
+using Turing.Core.Electricity;
+
+namespace Turing.Core.ComplexComponents;
+
+public class SINDEXER<T>(T input, Int shift) where T : struct, IByteValue<T>
+{
+    private readonly T _input = input;
+    private readonly Int _shift = shift;
+
+    public static implicit operator T(SINDEXER<T> indexer)
+    {
+        var negBit = indexer._shift.LastBit();
+
+        return new MUX<T>(
+            new LSR<T>(indexer._input, indexer._shift), 
+            new LSL<T>(indexer._input, 
+                new NEG<Int>(indexer._shift)
+            ), 
+            negBit
+        );
+    }
+}
---
+++ Turing.Tests/ComplexComponents/SINDEXERTests.cs
@@ -0,0 +1,22 @@
+﻿using Turing.Core.ComplexComponents;
+
+namespace Turing.Tests.ComplexComponents;
+
+[TestFixture]
+internal class SINDEXERTests
+{
+    [Test]
+    public void SINDEXER_ByteExhaustive_Returns_Correct_Output()
+    {
+        for (var i = 0; i < 256; i++)
+        {
+            for (var j = -8; j < 8; j++)
+            {
+                Byte actual = new SINDEXER<Byte>(i, j);
+                Byte expected = j < 0 ? i << -j : i >> j;
+
+                Assert.That(actual, Is.EqualTo(expected), $"{i}, {j}");
+            }
+        }
+    }
+}
