98557a0460da478548241d492a14e102a844ebf0
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-08-16 22:30:57 +0200

    add ram for memory

---
+++ Turing.Core/ComplexMemory/RAM.cs
@@ -0,0 +1,17 @@
+﻿namespace Turing.Core.ComplexMemory;
+
+public class RAM
+{
+    public static byte Off => throw new NotImplementedException();
+    private readonly Byte[] _state;
+
+    public RAM(params Byte[] instructions)
+    {
+        _state = new Byte[1 << Short.BitWidth];
+
+        instructions.CopyTo(_state);
+        _state.AsSpan(instructions.Length).Fill(Off);
+    }
+
+    public Byte EVal(Byte address) => _state[address];
+}
