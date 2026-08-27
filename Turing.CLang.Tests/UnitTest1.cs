2fa1312aa24fa09a8ae6d0f8f0924d7361fc4b0a
DESKTOP-VE3UNO0\Sebastian <sebastian.drela@wp.pl>
2026-08-27 02:11:10 +0200

    add clang proj

---
+++ Turing.CLang.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Turing.CLang.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
